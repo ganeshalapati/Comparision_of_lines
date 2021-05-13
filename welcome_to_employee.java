803db782ab4c868603681770e22d22335bf34a62
ganeshalapati <alapatiganesh31@gmail.com>
2021-05-13 22:06:56 +0530

    welcome message added

---
+++ welcome_to_employee.java
@@ -0,0 +1,10 @@
+
+public class welcome_to_employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		System.out.println("welome to line comparision");
+	}
+
+}
